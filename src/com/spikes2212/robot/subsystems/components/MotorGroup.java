7949547c5015e0382e418be2a45ea7d19547f15c
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2017-07-07 23:10:04 +0300

    wrote documentation for MotorGroup

--- src/com/spikes2212/robot/subsystems/components/MotorGroup.java
+++ src/com/spikes2212/robot/subsystems/components/MotorGroup.java
@@ -5,60 +5,107 @@ import edu.wpi.first.wpilibj.SpeedController;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * This class is used to wrap a number of {@link SpeedController} into one {@link SpeedController} interface
+ *
+ *
+ */
+
 public class MotorGroup implements SpeedController {
 
-    private ArrayList<SpeedController> motors;
+    /**
+     * An {@link ArrayList} that stores {@link SpeedController} instances
+     */
+    private ArrayList<SpeedController> controllers;
 
+    /**
+     * constructs a new ${@link MotorGroup} object
+     * that wraps a number of {@link SpeedController} objects as one
+     *
+     * @param controllers this parameter is an array of {@link SpeedController} objects
+     *                    the argument is vararg for more comfortable use
+     */
     public MotorGroup(SpeedController... controllers) {
-        motors = new ArrayList<>();
-        motors.addAll(Arrays.asList(controllers).subList(0, controllers.length - 1));
+        this.controllers = new ArrayList<>();
+        this.controllers.addAll(Arrays.asList(controllers).subList(0, controllers.length - 1));
     }
+
+    /**
+     *
+     * @return returns the speed of the speed controllers
+     */
 
     @Override
     public double get() {
-        if (motors.size() == 0)
+        if (controllers.size() == 0)
             return 0;
-        return motors.get(0).get();
+        return controllers.get(0).get();
     }
 
+    /**
+     *
+     * @param speed the speed the motor has to move at
+     */
     @Override
     public void set(double speed) {
-        for (int i = 0; i < motors.size()-1; i++) {
-            motors.get(i).set(speed);
+        for (int i = 0; i < controllers.size()-1; i++) {
+            controllers.get(i).set(speed);
         }
     }
+
+    /**
+     *
+     * @param isInverted makes all the speed Controllers inverted
+     */
 
     @Override
     public void setInverted(boolean isInverted) {
-        for (int i = 0; i < motors.size()-1; i++) {
-            motors.get(i).setInverted(isInverted);
+        for (int i = 0; i < controllers.size()-1; i++) {
+            controllers.get(i).setInverted(isInverted);
         }
     }
 
+    /**
+     *
+     * @return returns isInverted field of all the speed controllers
+     */
     @Override
     public boolean getInverted() {
-        if (motors.size() == 0) return false;
-        return motors.get(0).getInverted();
+        if (controllers.size() == 0) return false;
+        return controllers.get(0).getInverted();
     }
 
+    /**
+     * disables all the speed controllers
+     */
     @Override
     public void disable() {
-        for (int i = 0; i < motors.size()-1; i++) {
-            motors.get(i).disable();
+        for (int i = 0; i < controllers.size()-1; i++) {
+            controllers.get(i).disable();
         }
     }
 
+
+    /**
+     * stops the motor
+     */
     @Override
     public void stopMotor() {
-        for (int i = 0; i < motors.size()-1; i++) {
-            motors.get(i).stopMotor();
+        for (int i = 0; i < controllers.size()-1; i++) {
+            controllers.get(i).stopMotor();
         }
     }
 
+
+    /**
+     * Set the output to the value calculated by PIDController for all the speed controllers
+     *
+     * @param output the value calculated by PIDController
+     */
     @Override
     public void pidWrite(double output) {
-        for (int i = 0; i < motors.size()-1; i++) {
-            motors.get(i).pidWrite(output);
+        for (int i = 0; i < controllers.size()-1; i++) {
+            controllers.get(i).pidWrite(output);
         }
     }
 }
